dc0fa68b5c889efc04609fd758b53e6f40112c77
seyithantopal <seyithan_41_@hotmail.com>
2021-12-11 17:40:36 +0100

    Initialized the project

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+// Styles
+import { Wrapper } from './styles';
+
+const Home: FC = () => {
+  return (
+    <Wrapper>
+      <div>Home Page</div>
+    </Wrapper>
+  );
+};
+
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #eee;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+`
